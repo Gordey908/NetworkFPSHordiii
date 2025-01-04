460d678e897e4d30da8ffb485c3ad0517556b08d
Gordey908 <144526824+Gordey908@users.noreply.github.com>
2025-01-04 10:05:26 +0100

    lessons 17

--- Shooter with multiplayer/Assets/Scripts/ConnectionToServer.cs
+++ Shooter with multiplayer/Assets/Scripts/ConnectionToServer.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class ConnectionToServer : MonoBehaviourPunCallbacks
 {
+    public static ConnectionToServer Instance{get; private set; }
+    [SerializeField]
+    private TMP_InputField inputRoomName;
+
+
+    [SerializeField]
+    private TMP_Text roomName;
+
     private void Awake()
     {
+        Instance = this;
         PhotonNetwork.ConnectUsingSettings();
     }
 
@@ -19,5 +29,26 @@ public class ConnectionToServer : MonoBehaviourPunCallbacks
     {
         Debug.Log($"Connected To Lobby!");
         UIManager.Instance.OpenPanel("MenuPanel");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        UIManager.Instance.OpenPanel("GameRoomPanel");
+        roomName.text = PhotonNetwork.CurrentRoom.Name;
+    }
+    public override void OnLeftRoom()
+    {
+        UIManager.Instance.OpenPanel("MenuPanel");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public void CreateNewRoom()
+    {
+        if (string.IsNullOrEmpty(inputRoomName.text)) return;
+        PhotonNetwork.CreateRoom(inputRoomName.text);
     }
 }
